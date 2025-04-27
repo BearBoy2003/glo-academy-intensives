224e6c451e5bf87ec9c9292de6e824068df18065
Mikhail <sdbuh3@mail.ru>
2025-04-27 14:52:10 +0300

    feat(Ghostbusters): added tab switching between "Actors" and "Creators" and a slider arrow for news on mobile

--- Ghostbusters/js/main.js
+++ Ghostbusters/js/main.js
@@ -1,8 +1,43 @@
 $(document).ready(function () {
   const mMenuBtn = $(".m-menu-button");
   const mMenu = $(".m-menu");
+  const tab = $(".tab");
+
   mMenuBtn.on("click", function () {
     mMenu.toggleClass("active");
     $("body").toggleClass("no-scroll");
+  });
+
+  $(".tab").on("click", function () {
+    if ($(this).hasClass("active")) return;
+    $(".tab").removeClass("active");
+    $(this).addClass("active");
+    $(".tabs-content").removeClass("visible");
+    let activeTabContent = $(this).attr("data-target");
+    $(activeTabContent).addClass("visible");
+  });
+
+  var mySwiper = new Swiper(".swiper-container", {
+    slidesPerView: 4,
+    spaceBetween: 25,
+    loop: true,
+    keyboard: {
+      enabled: true,
+      onlyInViewport: true,
+    },
+    breakpoints: {
+      992: {
+        slidesPerView: 4,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      320: {
+        slidesPerView: 1,
+        navigation: {
+          nextEl: ".button-next",
+        },
+      },
+    },
   });
 });
